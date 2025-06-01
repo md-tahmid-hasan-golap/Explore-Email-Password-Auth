f3c49893b0f19dad99cf8d39d7f92fe195678770
md-tahmid-hasan-golap <golapraj47@gmail.com>
2025-06-01 23:23:00 +0600

    resolve

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter } from "react-router";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     Component: <Roots></Roots>,
+//   },
+// ]);
+
+createRoot(document.getElementById("root")).render(<StrictMode></StrictMode>);
